a98c7671c7a1be961e3b2c9c61b55531c46ecffa
pataro97 <pablo.tamayo.romero@gmail.com>
2020-08-26 02:48:23 +0200

    26.08.2020

--- src/app/lista/lista.page.ts
+++ src/app/lista/lista.page.ts
@@ -16,6 +16,7 @@ export class ListaPage implements OnInit {
   estadoLiq = "Bueno";
   UUID;
   nomLista;
+  fechaFinal;
   async ngOnInit() {
     // obtener nombres
     let url = this.router.url.replace('/lista/', '');
@@ -56,17 +57,35 @@ export class ListaPage implements OnInit {
         }
     // parte liquido
         // mensaje liquido en [menu] lentillas
-        if (this.dbUsuarios.data.diasLiq) {
+        if (this.dbUsuarios.data.fechIniC) {
           document.getElementById("mostrarDias").setAttribute("style", "visibility: visible; display: block");
           document.getElementById("menuLiq").setAttribute("style", "visibility: visible; display: block");
+         
+          // Calcular dias
+          var date = new Date(this.dbUsuarios.data.fechIniM+"/"+this.dbUsuarios.data.fechIniD+"/"+this.dbUsuarios.data.fechIniY);
+          let modDate = this.addDays(date, this.dbUsuarios.data.diasCad);
+
+          var dd = modDate.getDate();
+          var mm = modDate.getMonth() + 1;
+          var y = modDate.getFullYear();
+
+          this.fechaFinal = dd+'/'+mm+'/'+y;
+
+
         } else {
           document.getElementById("mostrarError").setAttribute("style", "visibility: visible; display: block");
           document.getElementById("menuIniLiq").setAttribute("style", "visibility: visible; display: block");
+          document.getElementById("botCad").setAttribute("style", "visibility: hidden; display: none");
         }
 
   }
 
- 
+
+  addDays(date: Date, days: number): Date {
+    date.setDate(date.getDate() + days);
+    return date;
+}
+
 
   hiddenElement(nombre) {
     // mostrar datos
@@ -144,6 +163,26 @@ export class ListaPage implements OnInit {
 
   }
 
+  changeDate() {
+    // mostrar resto de campos
+    document.getElementById("botCad").setAttribute("style", "visibility: visible; display: block");
 
+  }
+
+  async comenzarLiq(date, cad) {
+    // extraer y separar fecha
+    let ArrayDate = date.split("-", 3);
+    let año = ArrayDate[0];
+    let mes = ArrayDate[1];
+    let dia = ArrayDate[2].substring(0, 2);
+    // escribir datos
+    this.dbUsuarios.data.fechIniC = dia+"/"+mes+"/"+año;
+    this.dbUsuarios.data.fechIniD = dia;
+    this.dbUsuarios.data.fechIniM = mes;
+    this.dbUsuarios.data.fechIniY = año;
+    this.dbUsuarios.data.diasCad = cad;
+    await this.firestoreService.actualizar('usuarios', this.UUID+";"+this.nomLista, this.dbUsuarios.data)
+    this.dataManagement();
+  }
 
 }
