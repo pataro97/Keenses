001b8e0dab8f90db5439393ccdd3dde682ca729d
pataro97 <pablo.tamayo.romero@gmail.com>
2020-08-26 04:12:41 +0200

    26.08.2020

--- src/app/lista/lista.page.ts
+++ src/app/lista/lista.page.ts
@@ -60,10 +60,10 @@ export class ListaPage implements OnInit {
         if (this.dbUsuarios.data.fechIniC) {
           document.getElementById("mostrarDias").setAttribute("style", "visibility: visible; display: block");
           document.getElementById("menuLiq").setAttribute("style", "visibility: visible; display: block");
-         
-          // Calcular dias
+
+          // ------------------ Calcular dias
           var date = new Date(this.dbUsuarios.data.fechIniM+"/"+this.dbUsuarios.data.fechIniD+"/"+this.dbUsuarios.data.fechIniY);
-          let modDate = this.addDays(date, this.dbUsuarios.data.diasCad);
+          let modDate = this.addDays(date, this.dbUsuarios.data.diasCadO);
 
           var dd = modDate.getDate();
           var mm = modDate.getMonth() + 1;
@@ -72,12 +72,38 @@ export class ListaPage implements OnInit {
           this.fechaFinal = dd+'/'+mm+'/'+y;
 
 
+
+          // --------------------------- restar dias
+          let dateNow = new Date();
+          let dia =  dateNow.getDate();
+          let mes = dateNow.getMonth()+1;
+          let año = dateNow.getFullYear();
+          let fechaCompletaActual = dia+'/'+mes+'/'+año;
+                    
+          
+          if(this.restaFechas(fechaCompletaActual, this.fechaFinal) != this.dbUsuarios.data.diasCad) {
+            
+            this.dbUsuarios.data.diasCad = this.restaFechas(fechaCompletaActual, this.fechaFinal);
+            this.firestoreService.actualizar('usuarios', this.UUID+";"+this.nomLista, this.dbUsuarios.data)
+          }
+
+
         } else {
           document.getElementById("mostrarError").setAttribute("style", "visibility: visible; display: block");
           document.getElementById("menuIniLiq").setAttribute("style", "visibility: visible; display: block");
           document.getElementById("botCad").setAttribute("style", "visibility: hidden; display: none");
         }
 
+  }
+
+  restaFechas = function(f1,f2) {
+    var aFecha1 = f1.split('/');
+    var aFecha2 = f2.split('/');
+    var fFecha1 = Date.UTC(aFecha1[2],aFecha1[1]-1,aFecha1[0]);
+    var fFecha2 = Date.UTC(aFecha2[2],aFecha2[1]-1,aFecha2[0]);
+    var dif = fFecha2 - fFecha1;
+    var dias = Math.floor(dif / (1000 * 60 * 60 * 24));
+    return dias;
   }
 
 
@@ -96,7 +122,22 @@ export class ListaPage implements OnInit {
 
   async comenzarLent() {
     let date = new Date();
-    let datos = {dia: date.getDate(), mes: date.getMonth()+1, year: date.getFullYear(), estado: "Bueno", diasUso: 0}
+
+    let datos;
+    if(this.dbUsuarios.data.fechIniD == null ) {
+      datos = {dia: date.getDate(), mes: date.getMonth()+1, year: date.getFullYear(), estado: "Bueno", diasUso: 0}
+    } else {
+      datos = {dia: date.getDate(), mes: date.getMonth()+1, year: date.getFullYear(), estado: "Bueno", diasUso: 0, fechIniC: this.dbUsuarios.data.fechIniC,
+      fechIniD: this.dbUsuarios.data.fechIniD,
+      fechIniM: this.dbUsuarios.data.fechIniM,
+      fechIniY: this.dbUsuarios.data.fechIniY,
+      diasCadO: this.dbUsuarios.data.diasCadO,
+      diasCad: this.dbUsuarios.data.diasCad
+    }
+  
+
+    }
+
     await this.firestoreService.actualizar('usuarios', this.UUID+";"+this.nomLista, datos);
     // ocultar mensaje
     document.getElementById('mensajeNoLentillas').setAttribute("style", "visibility: hidden; display: none");
@@ -180,8 +221,11 @@ export class ListaPage implements OnInit {
     this.dbUsuarios.data.fechIniD = dia;
     this.dbUsuarios.data.fechIniM = mes;
     this.dbUsuarios.data.fechIniY = año;
+    this.dbUsuarios.data.diasCadO = cad;
     this.dbUsuarios.data.diasCad = cad;
     await this.firestoreService.actualizar('usuarios', this.UUID+";"+this.nomLista, this.dbUsuarios.data)
+    document.getElementById("menuIniLiq").setAttribute("style", "visibility: hidden; display: none");
+    document.getElementById("mostrarError").setAttribute("style", "visibility: hidden; display: none");
     this.dataManagement();
   }
 
